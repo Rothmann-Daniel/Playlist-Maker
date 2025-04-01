3de15584f176aac70c08776f6730235f94c1d064
Daniel Rotman <danilarothmann@gmail.com>
2025-04-01 14:01:44 +0700

    design of the "Home screen" and the "Settings" screen for the night without refactoring

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,14 +1,31 @@
 package com.example.playlistmaker
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.graphics.drawable.DrawableCompat.applyTheme
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings) // Установка разметки
+        val themeSwitch = findViewById<SwitchMaterial>(R.id.switch_theme)
+        val sharedPrefs = getSharedPreferences("AppPrefs", MODE_PRIVATE)
+
+        // Установка текущего состояния
+        themeSwitch.isChecked = isDarkThemeEnabled()
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            sharedPrefs.edit().putBoolean("dark_theme", isChecked).apply()
+            applyTheme(isChecked)
+        }
+
+
+
 
         //Обработчик нажатия на кнопку навигаци: Назад
         val navBack = findViewById<MaterialToolbar>(R.id.tool_bar)
@@ -16,5 +33,23 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+    private fun applyTheme(isDarkTheme: Boolean) {
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkTheme) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+        recreate() // Пересоздаем активити для применения темы
+    }
+    private fun isDarkThemeEnabled(): Boolean {
+        return when (AppCompatDelegate.getDefaultNightMode()) {
+            AppCompatDelegate.MODE_NIGHT_YES -> true
+            AppCompatDelegate.MODE_NIGHT_NO -> false
+            else -> {
+                // Если тема системы, проверяем системные настройки
+                resources.configuration.uiMode and
+                        Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+            }
+        }
     }
 }
