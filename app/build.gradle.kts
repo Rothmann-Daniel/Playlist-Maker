fa0cd33c72bd0ad73a2297bafe3dd0e11ba434cd
Daniel Rotman <danilarothmann@gmail.com>
2025-04-01 21:01:48 +0700

    added implementation Share the app Write to support User agreement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.browser:browser:1.8.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
