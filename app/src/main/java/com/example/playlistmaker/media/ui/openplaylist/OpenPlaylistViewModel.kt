ab3eb06009dad89a7039371439fcceeead7322c7
Daniel Rotman <danilarothmann@gmail.com>
2025-10-23 16:34:05 +0700

    Sprint No.23 Step2v1 A Playlist information display screen has been created.

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/openplaylist/OpenPlaylistViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.media.ui.openplaylist
+
+import androidx.lifecycle.ViewModel
+
+class OpenPlaylistViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
